291000866892383c14dda9d78ada29a754c3023a
Matthew Houston <matthouston027@gmail.com>
2023-03-10 20:08:14 +0900

    Add component folder and first working component in React

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible</p>
+      <p>This too is visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
