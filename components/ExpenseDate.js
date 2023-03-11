3a54f2ce0d8d972ce2c991b052fac73428bbb762
Matthew Houston <matthouston027@gmail.com>
2023-03-11 22:45:53 +0900

    fix props import in ExpenseDate file

--- components/ExpenseDate.js
+++ components/ExpenseDate.js
@@ -1,9 +1,12 @@
 function ExpenseDate(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
     return (
         <div>
-            <div>{props.month}</div>
-            <div>{props.day}</div>
-            <div>{props.year}</div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
         </div>
 
     );
--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -2,12 +2,10 @@ import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString('en-US', { month: 'long' });
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-    const year = props.date.getFullYear();
+
     return (
         <div className='expense-item'>
-            <ExpenseDate month={month} day={day} year={year}>
+            <ExpenseDate date={props.date}>
 
             </ExpenseDate>
             <div className='expense-item__description'>
