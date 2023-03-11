421d90334c1c027936fda237f53e690d162b49c3
Matthew Houston <matthouston027@gmail.com>
2023-03-11 22:39:08 +0900

    separate date div into separate component

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import "./ExpenseItem.css"
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
     return (
         <div className='expense-item'>
-            <div>
-                {props.date.toISOString()}
-            </div>
+            <ExpenseDate month={month} day={day} year={year}>
+
+            </ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount} </div>
