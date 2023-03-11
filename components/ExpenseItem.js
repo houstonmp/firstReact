4ff5c8b3f68759330036e0220925f0b439010d5c
Matthew Houston <matthouston027@gmail.com>
2023-03-11 22:13:00 +0900

    Follow Max course, add CSS and expense data

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,6 +1,16 @@
-function ExpenseItem() {
+import "./ExpenseItem.css"
+
+function ExpenseItem(props) {
     return (
-        <h2>Expense Item!</h2>
+        <div className='expense-item'>
+            <div>
+                {props.date.toISOString()}
+            </div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount} </div>
+            </div>
+        </div>
     );
 }
 
