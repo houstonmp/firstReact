eeb22abcb6f4ca84c487c7d2a25de81914d06bb2
Matthew Houston <matthouston027@gmail.com>
2023-03-26 20:08:04 +0900

    Select year and pass it up to Expenses.js and print it in console

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -1,6 +1,8 @@
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import "./Expenses.css"
+import "./ExpenseFilter.css"
+import ExpenseFilter from './ExpenseFilter';
 
 function Expenses(props) {
 
@@ -14,14 +16,23 @@ function Expenses(props) {
 
     // }
 
+    const yearHandler = (selectedYear) => {
+        console.log('In Expense.js:', selectedYear);
+    }
+
     return (
-        <Card className="expenses">
-            {/* {renderExpenseItem()} */}
-            <ExpenseItem title={props.expenseArr[0].title} amount={props.expenseArr[0].amount} date={props.expenseArr[0].date}></ExpenseItem>
-            <ExpenseItem title={props.expenseArr[1].title} amount={props.expenseArr[1].amount} date={props.expenseArr[1].date}></ExpenseItem>
-            <ExpenseItem title={props.expenseArr[2].title} amount={props.expenseArr[2].amount} date={props.expenseArr[2].date}></ExpenseItem>
-            <ExpenseItem title={props.expenseArr[3].title} amount={props.expenseArr[3].amount} date={props.expenseArr[3].date}></ExpenseItem>
-        </Card>
+        <div>
+
+
+            <Card className="expenses">
+                <ExpenseFilter className="expenses-filter" onSelectYear={yearHandler} />
+                {/* {renderExpenseItem()} */}
+                <ExpenseItem title={props.expenseArr[0].title} amount={props.expenseArr[0].amount} date={props.expenseArr[0].date}></ExpenseItem>
+                <ExpenseItem title={props.expenseArr[1].title} amount={props.expenseArr[1].amount} date={props.expenseArr[1].date}></ExpenseItem>
+                <ExpenseItem title={props.expenseArr[2].title} amount={props.expenseArr[2].amount} date={props.expenseArr[2].date}></ExpenseItem>
+                <ExpenseItem title={props.expenseArr[3].title} amount={props.expenseArr[3].amount} date={props.expenseArr[3].date}></ExpenseItem>
+            </Card>
+        </div>
     );
 }
 
