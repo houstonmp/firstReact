88732b84c8711cbbb1307392083cd1f8f60ff4e5
Matthew Houston <matthouston027@gmail.com>
2023-03-28 23:00:46 +0900

    Add conditional via terniary exp that check to see if array indices exist or not

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -13,26 +13,26 @@ function Expenses(props) {
         console.log('In Expense.js:', selectedYear);
     }
 
+    const filteredExpenses = props.expenseArr.filter((el) => {
+        //return only data of the chosenYear
+        const date = new Date(el.date).getFullYear();
+        return parseInt(chosenYear) === date;
+    });
+
     const renderExpenseItem = (expenses) => {
-        return props.expenseArr
-            .filter((el) => {
-                //return only data of the chosenYear
-                const date = new Date(el.date).getFullYear();
-                return parseInt(chosenYear) === date;
-            })
-            .map((el) => {
-                //Create an array of ExpenseItems of chosenYear
-                const current =
-                    <ExpenseItem
-                        key={el.id}
-                        id={el.id}
-                        title={el.title}
-                        amount={el.amount}
-                        date={el.date}
-                    />;
-                console.log(`Making ${el.id},${el.title},${el.amount},${el.date}`);
-                return current;
-            })
+        return expenses.map((el) => {
+            //Create an array of ExpenseItems of chosenYear
+            const current =
+                <ExpenseItem
+                    key={el.id}
+                    id={el.id}
+                    title={el.title}
+                    amount={el.amount}
+                    date={el.date}
+                />;
+            console.log(current);
+            return current;
+        })
     }
 
     return (
@@ -40,7 +40,8 @@ function Expenses(props) {
             <div>
                 <ExpenseFilter selected={chosenYear} onSelectYear={yearHandler} />
             </div>
-            {renderExpenseItem()}
+            {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+            {filteredExpenses.length > 0 && renderExpenseItem(filteredExpenses)}
         </Card>
     );
 }
