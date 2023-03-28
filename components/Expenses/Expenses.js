c76c263b0df19495cc8df356d3bd5b1fd0ae66f7
Matthew Houston <matthouston027@gmail.com>
2023-03-28 19:50:10 +0900

    Add ExpenseItem components dynamically via map function

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -8,7 +8,7 @@ import ExpenseFilter from './ExpenseFilter';
 function Expenses(props) {
     const [chosenYear, setYear] = useState('2020');
 
-    //Attempt to add ExpenseItem components dynamically
+    // Attempt to add ExpenseItem components dynamically
     // const renderExpenseItem = () => {
     //     return props.expenseArr.map((el) => {
     //         const current = <ExpenseItem id={el.id} title={el.title
@@ -28,11 +28,20 @@ function Expenses(props) {
             <div>
                 <ExpenseFilter selected={chosenYear} onSelectYear={yearHandler} />
             </div>
-            {/* {renderExpenseItem()} */}
-            <ExpenseItem title={props.expenseArr[0].title} amount={props.expenseArr[0].amount} date={props.expenseArr[0].date}></ExpenseItem>
+            {props.expenseArr.map((el) => {
+                const current =
+                    <ExpenseItem
+                        id={el.id}
+                        title={el.title}
+                        amount={el.amount}
+                        date={el.date}
+                    />;
+                return current;
+            })}
+            {/* <ExpenseItem title={props.expenseArr[0].title} amount={props.expenseArr[0].amount} date={props.expenseArr[0].date}></ExpenseItem>
             <ExpenseItem title={props.expenseArr[1].title} amount={props.expenseArr[1].amount} date={props.expenseArr[1].date}></ExpenseItem>
             <ExpenseItem title={props.expenseArr[2].title} amount={props.expenseArr[2].amount} date={props.expenseArr[2].date}></ExpenseItem>
-            <ExpenseItem title={props.expenseArr[3].title} amount={props.expenseArr[3].amount} date={props.expenseArr[3].date}></ExpenseItem>
+            <ExpenseItem title={props.expenseArr[3].title} amount={props.expenseArr[3].amount} date={props.expenseArr[3].date}></ExpenseItem> */}
         </Card>
     );
 }
